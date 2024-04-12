943da8114177642f1d327f56d115fa9bfcb33d13
guohao <1163753605@qq.com>
2024-04-12 12:59:58 +0800

    手撕bert代码+面经

--- 02_bert/bert.py
+++ 02_bert/bert.py
@@ -121,5 +121,154 @@ class MyDataSet(Data.Dataset):
 
 loader = Data.DataLoader(MyDataSet(input_ids, segment_ids, masked_tokens, masked_pos, isNext), batch_size, True)
 
-print(loader)
 
+def get_attn_pad_mask(seq_q, seq_k):
+    batch_size, seq_len = seq_q.size()
+    # eq(zero) is PAD token
+    pad_attn_mask = seq_q.data.eq(0).unsqueeze(1)  # [batch_size, 1, seq_len]
+    return pad_attn_mask.expand(batch_size, seq_len, seq_len)  # [batch_size, seq_len, seq_len]
+
+def gelu(x):
+    """
+      Implementation of the gelu activation function.
+      For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
+      0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+      Also see https://arxiv.org/abs/1606.08415
+    """
+    return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
+class Embedding(nn.Module):
+    def __init__(self):
+        super(Embedding, self).__init__()
+        self.tok_embed = nn.Embedding(vocab_size, d_model)  # token embedding
+        self.pos_embed = nn.Embedding(maxlen, d_model)  # position embedding
+        self.seg_embed = nn.Embedding(n_segments, d_model)  # segment(token type) embedding
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, x, seg):
+        seq_len = x.size(1)
+        pos = torch.arange(seq_len, dtype=torch.long)
+        pos = pos.unsqueeze(0).expand_as(x)  # [seq_len] -> [batch_size, seq_len]
+        embedding = self.tok_embed(x) + self.pos_embed(pos) + self.seg_embed(seg)
+        return self.norm(embedding)
+
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask):
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k) # scores : [batch_size, n_heads, seq_len, seq_len]
+        scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is one.
+        attn = nn.Softmax(dim=-1)(scores)
+        context = torch.matmul(attn, V)
+        return context
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super(MultiHeadAttention, self).__init__()
+        self.W_Q = nn.Linear(d_model, d_k * n_heads)
+        self.W_K = nn.Linear(d_model, d_k * n_heads)
+        self.W_V = nn.Linear(d_model, d_v * n_heads)
+    def forward(self, Q, K, V, attn_mask):
+        # q: [batch_size, seq_len, d_model], k: [batch_size, seq_len, d_model], v: [batch_size, seq_len, d_model]
+        residual, batch_size = Q, Q.size(0)
+        # (B, S, D) -proj-> (B, S, D) -split-> (B, S, H, W) -trans-> (B, H, S, W)
+        q_s = self.W_Q(Q).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # q_s: [batch_size, n_heads, seq_len, d_k]
+        k_s = self.W_K(K).view(batch_size, -1, n_heads, d_k).transpose(1,2)  # k_s: [batch_size, n_heads, seq_len, d_k]
+        v_s = self.W_V(V).view(batch_size, -1, n_heads, d_v).transpose(1,2)  # v_s: [batch_size, n_heads, seq_len, d_v]
+
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1) # attn_mask : [batch_size, n_heads, seq_len, seq_len]
+
+        # context: [batch_size, n_heads, seq_len, d_v], attn: [batch_size, n_heads, seq_len, seq_len]
+        context = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v) # context: [batch_size, seq_len, n_heads * d_v]
+        output = nn.Linear(n_heads * d_v, d_model)(context)
+        return nn.LayerNorm(d_model)(output + residual) # output: [batch_size, seq_len, d_model]
+
+class PoswiseFeedForwardNet(nn.Module):
+    def __init__(self):
+        super(PoswiseFeedForwardNet, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        # (batch_size, seq_len, d_model) -> (batch_size, seq_len, d_ff) -> (batch_size, seq_len, d_model)
+        return self.fc2(gelu(self.fc1(x)))
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+        self.enc_self_attn = MultiHeadAttention()
+        self.pos_ffn = PoswiseFeedForwardNet()
+
+    def forward(self, enc_inputs, enc_self_attn_mask):
+        enc_outputs = self.enc_self_attn(enc_inputs, enc_inputs, enc_inputs, enc_self_attn_mask) # enc_inputs to same Q,K,V
+        enc_outputs = self.pos_ffn(enc_outputs) # enc_outputs: [batch_size, seq_len, d_model]
+        return enc_outputs
+
+class BERT(nn.Module):
+    def __init__(self):
+        super(BERT, self).__init__()
+        self.embedding = Embedding()
+        self.layers = nn.ModuleList([EncoderLayer() for _ in range(n_layers)])
+        self.fc = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.Dropout(0.5),
+            nn.Tanh(),
+        )
+        self.classifier = nn.Linear(d_model, 2)   # 二分类
+        self.linear = nn.Linear(d_model, d_model)
+        self.activ2 = gelu
+        # fc2 is shared with embedding layer
+        embed_weight = self.embedding.tok_embed.weight
+        self.fc2 = nn.Linear(d_model, vocab_size, bias=False)
+        self.fc2.weight = embed_weight
+
+    def forward(self, input_ids, segment_ids, masked_pos):
+        output = self.embedding(input_ids, segment_ids)  # [bach_size, seq_len, d_model]
+        enc_self_attn_mask = get_attn_pad_mask(input_ids, input_ids)  # [batch_size, maxlen, maxlen]
+        for layer in self.layers:
+            # output: [batch_size, max_len, d_model]
+            output = layer(output, enc_self_attn_mask)
+        # it will be decided by first token(CLS)
+        h_pooled = self.fc(output[:, 0])  # [batch_size, d_model]  因为 output[:, 0] 表示取每个样本的第一个位置的输出，即在第二维上取索引为0的切片，因此会将原来的30个token的输出维度合并为一个，而每个token的输出维度是768，所以最终输出的维度是 (6, 768)，
+        logits_clsf = self.classifier(h_pooled)  # [batch_size, 2] predict isNext
+
+        masked_pos = masked_pos[:, :, None].expand(-1, -1, d_model)  # [batch_size, max_pred, d_model]
+        h_masked = torch.gather(output, 1, masked_pos)  # masking position [batch_size, max_pred, d_model]
+        h_masked = self.activ2(self.linear(h_masked))  # [batch_size, max_pred, d_model]
+        logits_lm = self.fc2(h_masked)  # [batch_size, max_pred, vocab_size]
+        return logits_lm, logits_clsf
+model = BERT()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adadelta(model.parameters(), lr=0.001)
+
+
+# train and test
+for epoch in range(180):
+    for input_ids, segment_ids, masked_tokens, masked_pos, isNext in loader:
+      logits_lm, logits_clsf = model(input_ids, segment_ids, masked_pos)
+      loss_lm = criterion(logits_lm.view(-1, vocab_size), masked_tokens.view(-1))  # for masked LM
+      loss_lm = (loss_lm.float()).mean()
+      loss_clsf = criterion(logits_clsf, isNext)  # for sentence classification
+      loss = loss_lm + loss_clsf
+      if (epoch + 1) % 10 == 0:
+          print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+# Predict mask tokens ans isNext
+input_ids, segment_ids, masked_tokens, masked_pos, isNext = batch[0]
+print(text)
+print([idx2word[w] for w in input_ids if idx2word[w] != '[PAD]'])
+
+logits_lm, logits_clsf = model(torch.LongTensor([input_ids]), \
+                 torch.LongTensor([segment_ids]), torch.LongTensor([masked_pos]))
+logits_lm = logits_lm.data.max(2)[1][0].data.numpy()
+print('masked tokens list : ',[pos for pos in masked_tokens if pos != 0])
+print('predict masked tokens list : ',[pos for pos in logits_lm if pos != 0])
+
+logits_clsf = logits_clsf.data.max(1)[1].data.numpy()[0]
+print('isNext : ', True if isNext else False)
+print('predict isNext : ', True if logits_clsf else False)
